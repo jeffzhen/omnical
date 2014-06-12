2465f6abe4197acb35ec78c68e2670515d57f83e
Jeff Zheng <jeff_z@mit.edu>
2014-06-12 17:12:39 -0400

    incoorpoarted new RedundantCalibrator class in the test.py file

--- test.py
+++ test.py
@@ -14,13 +14,15 @@ wantpols = {'xx':-5, 'yy':-6}
 infopaths = {'xx':'./redundantinfo_PSA32.txt', 'yy':'./redundantinfo_PSA32.txt'}
 oppath = './results/'
 
-removedegen = 1
-use_logcal = 1
+removedegen = True
+removeadditive = False
 
 needrawcal = True #if true, (generally true for raw data) you need to take care of having raw calibration parameters in float32 binary format freq x nant
 rawpaths = {'xx':"testrawphasecalparrad_xx", 'yy':"testrawphasecalparrad_yy"}
 
 keep_binary_data = True
+keep_binary_calpar = True
+
 
 converge_percent = 0.01
 max_iter = 10
@@ -33,10 +35,6 @@ step_size = .3
 ########Massage user parameters###################################
 oppath += '/'
 
-####read redundant info################
-info = [omni.read_redundantinfo(infopaths[key]) for key in wantpols.keys()]
-#print info[0]['bl1dmatrix']
-#exit(1)
 
 ####get some info from the first uvfile   ################
 uv=ap.miriad.UV(uvfiles[0])
@@ -46,20 +44,28 @@ startfreq = uv['sfreq']
 dfreq = uv['sdf']
 del(uv)
 
+
+####create redundant calibrators################
+calibrators = [omni.RedundantCalibrator(nant, info = infopaths[key]) for key in wantpols.keys()]
+#print info[0]['bl1dmatrix']
+#exit(1)
+
+
+
 ###read raw phase calibration prameters over frequencyfor each antenna, 203 by 32 in radiants; this part can be replaced################
 if needrawcal:
 	rawcalpar = np.asarray([np.fromfile(rawpaths[key], dtype="complex64").reshape(nfreq, nant) for key in wantpols.keys()])
 	rawcorrection = np.zeros((len(wantpols), nfreq, nant*(nant+1)/2), dtype='complex64') + 1#to be dividing the data;  data/calpar = model
 	for p in range(len(wantpols)):
-		for i, bl in zip(range(len(info[p]['subsetbl'])), info[p]['subsetbl']):
-			a1, a2 = info[p]['bl2d'][i]
+		for i, bl in zip(range(len(calibrators[p].info['subsetbl'])), calibrators[p].info['subsetbl']):
+			a1, a2 = calibrators[p].info['bl2d'][i]
 			rawcorrection[p, :, bl] = np.conj(rawcalpar[p, :,a1]) *  rawcalpar[p, :,a2]
 
 
 
 ###start reading miriads################
 print FILENAME + " MSG:",  len(uvfiles), "uv files to be processed for " + ano
-data, t, timing, lst = omni.importuvs(uvfiles, info, wantpols)
+data, t, timing, lst = omni.importuvs(uvfiles, [calibrator.info for calibrator in calibrators], wantpols)
 print FILENAME + " MSG:",  len(t), "slices read.",
 
 ###reorder and dump the binary data from miriad################
@@ -86,24 +92,36 @@ for l in lst:
 f.close()
 del(data)
 
-####Call C++ omnical code################
-for p, pol in zip(range(len(wantpols)), wantpols.keys()):
-	command = "./omnical " + oppath + 'miriadextract_' + pol + '_' + ano + " " + infopaths[pol] + " " + str(len(t)) + " " + str(nfreq) + " "  + str(nant) + " " + str(removedegen) + " 0 0 " + str(use_logcal) + " " + str(converge_percent) + " " + str(max_iter) + " " + str(step_size)
-	print FILENAME + " MSG: System call: ",  command
-	os.system(command)
-	print np.fromfile(oppath + 'miriadextract_' + pol + '_' + ano + ".omnical", dtype = 'float32').reshape((len(t), nfreq, 3+2*(info[p]['nAntenna']+info[p]['nUBL'])))[:5,50,:3]
-	if not keep_binary_data:
-		os.remove(oppath + 'miriadextract_' + pol + '_' + ano)
+####calibrate################
+for pol, calibrator in zip(wantpols.keys(), calibrators):
+	calibrator.nTime = len(t)
+	calibrator.nFrequency = nfreq
+	calibrator.removeDegeneracy = removedegen
+	calibrator.removeAdditive = removeadditive
+	calibrator.keepData = keep_binary_data
+	calibrator.keepCalpar = keep_binary_calpar
+	calibrator.convergePercent = converge_percent
+	calibrator.maxIteration = max_iter
+	calibrator.stepSize = step_size
+	calibrator.loglincal(oppath + 'miriadextract_' + pol + '_' + ano)
 
 
 #########Test results############
-newresult = np.fromfile(oppath + "miriadextract_xx_test.omnical", dtype = 'float32')
 correctresult = np.fromfile("test.omnical", dtype = 'float32')
+
+newresult = calibrators[1].rawCalpar.flatten()#np.fromfile(oppath + "miriadextract_xx_test.omnical", dtype = 'float32')
 if (len(newresult) == len(correctresult)) and ((newresult == correctresult) | (np.isnan(newresult) & np.isnan(correctresult))).all():
 	print "TEST PASSED!"
 else:
 	print "TEST FAILED :("
-for p, pol in zip(range(len(wantpols)), wantpols.keys()):
-	os.remove(oppath + 'miriadextract_' + pol + '_' + ano + '.omnical')
+
+newresult = np.fromfile(oppath + "miriadextract_xx_test.omnical", dtype = 'float32')
+if (len(newresult) == len(correctresult)) and ((newresult == correctresult) | (np.isnan(newresult) & np.isnan(correctresult))).all():
+	print "TEST PASSED!"
+else:
+	print "TEST FAILED :("
+
+#for p, pol in zip(range(len(wantpols)), wantpols.keys()):
+	#os.remove(oppath + 'miriadextract_' + pol + '_' + ano + '.omnical')
 os.remove(oppath + 'miriadextract_' + ano + "_localtime.dat")
 os.remove(oppath + 'miriadextract_' + ano + "_lsthour.dat")
