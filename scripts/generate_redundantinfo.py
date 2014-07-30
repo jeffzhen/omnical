f60adc9c248b7d9ee4f447631fe7e61ef7e82fc3
Jeff Zheng <jeff_z@mit.edu>
2014-07-30 17:53:02 -0400

    minor modification to generate_redundantinfo.py

--- scripts/generate_redundantinfo.py
+++ scripts/generate_redundantinfo.py
@@ -28,40 +28,41 @@ class RedundantCalibrator_PAPER(omni.RedundantCalibrator):
 
 
 
+if __name__ == '__main__':
+	######################################################################
+	##############Config parameters###################################
+	######################################################################
 
-######################################################################
-##############Config parameters###################################
-######################################################################
-o = optparse.OptionParser()
+	o = optparse.OptionParser()
 
-ap.scripting.add_standard_options(o, cal=True, pol=False)
-o.add_option('-o', '--path', action = 'store', default = '', help = 'output name with path')
-o.add_option('-e', '--tol', action = 'store', type = 'float', default = 1e-2, help = 'tolerance of antenna location deviation when computing unique baselines.')
-o.add_option('--ba', action = 'store', default = '', help = 'bad antenna number indices seperated by commas')
-o.add_option('--bu', action = 'store', default = '', help = 'bad unique baseline indices seperated by commas')
-o.add_option('--overwrite', action = 'store_true', help = 'overwrite if file exists')
-opts,args = o.parse_args(sys.argv[1:])
+	ap.scripting.add_standard_options(o, cal=True, pol=False)
+	o.add_option('-o', '--path', action = 'store', default = '', help = 'output name with path')
+	o.add_option('-e', '--tol', action = 'store', type = 'float', default = 1e-2, help = 'tolerance of antenna location deviation when computing unique baselines.')
+	o.add_option('--ba', action = 'store', default = '', help = 'bad antenna number indices seperated by commas')
+	o.add_option('--bu', action = 'store', default = '', help = 'bad unique baseline indices seperated by commas')
+	o.add_option('--overwrite', action = 'store_true', help = 'overwrite if file exists')
+	opts,args = o.parse_args(sys.argv[1:])
 
-if opts.path == '':
-	raise Exception('Error: no output filename specified! Use -o to specify full name and path.')
-#if os.path.isfile(opts.path):
-	#raise Exception('Error: output filename exists!')
+	if opts.path == '':
+		raise Exception('Error: no output filename specified! Use -o to specify full name and path.')
+	#if os.path.isfile(opts.path):
+		#raise Exception('Error: output filename exists!')
 
-aa = ap.cal.get_aa(opts.cal, np.array([.15]))
+	aa = ap.cal.get_aa(opts.cal, np.array([.15]))
 
-try:
-	badAntenna = [int(i) for i in opts.ba.split(',')]
-except:
-	badAntenna = []
-try:
-	badUBL = [int(i) for i in opts.bu.split(',')]
-except:
-	badUBL = []
-print 'Bad Antennas:', badAntenna
-print 'Bad unique baselines:', badUBL
+	try:
+		badAntenna = [int(i) for i in opts.ba.split(',')]
+	except:
+		badAntenna = []
+	try:
+		badUBL = [int(i) for i in opts.bu.split(',')]
+	except:
+		badUBL = []
+	print 'Bad Antennas:', badAntenna
+	print 'Bad unique baselines:', badUBL
 
-calibrator = RedundantCalibrator_PAPER(aa)
-calibrator.compute_redundantinfo(badAntenna = badAntenna, badUBL = badUBL, antennaLocationTolerance = opts.tol)
-calibrator.write_redundantinfo(infoPath = opts.path, overwrite = opts.overwrite, verbose = True)
+	calibrator = RedundantCalibrator_PAPER(aa)
+	calibrator.compute_redundantinfo(badAntenna = badAntenna, badUBL = badUBL, antennaLocationTolerance = opts.tol)
+	calibrator.write_redundantinfo(infoPath = opts.path, overwrite = opts.overwrite, verbose = True)
 
 
