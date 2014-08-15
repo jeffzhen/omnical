7d06d3a8d4d85d9ce10f742899fb436ccedcba5a
Jeff Zheng <jeff_z@mit.edu>
2014-08-15 16:59:58 -0400

    batch script updated

--- scripts/batch_omnical.sh
+++ scripts/batch_omnical.sh
@@ -4,7 +4,7 @@
 #$ -e grid_output
 #$ -o grid_output
 #$ -l paper
-#$ -l h_vmem=7G
+#$ -l h_vmem=5G
 DIRS=`pull_args.py $*`
 CALFILE=psa6240_v003
 EXT=uvcRREcAC
@@ -16,9 +16,9 @@ for dir in ${DIRS}; do
     echo TAG2 ${TAG2}
     echo ==============================================================================================================================
     for CHUNK in `seq 1 1 6`; do
-        echo ./omnical_PSA64.py -C ${CALFILE} -p xx,yy --tag=${TAG1}_245${TAG2}.${CHUNK} $dir/zen.*.${CHUNK}*.${EXT} --skip --add --nadd 7
+        echo python scripts/omnical_PSA128.py $dir/zen.*.${CHUNK}*.${EXT} -C ${CALFILE} -p xx,yy -t aug2014  -d ${TAG1}_245${TAG2}.${CHUNK} -i doc/redundantinfo_PSA64_7ba_7bu_08-15-2014.bin --datapath /data2/home/hz2ug/omnical_old/results  -o /data4/paper/2012EoR/psa_live/PSA64_omnical_results_Aug_2014  --health 3,4 -k
         echo ----------------------------------------------------------------------------------------------------------------------------
-        ./omnical_PSA64.py -C ${CALFILE} -p xx,yy --tag=${TAG1}_245${TAG2}.${CHUNK} $dir/zen.*.${CHUNK}*.${EXT} --skip --add --nadd 7
+        python scripts/omnical_PSA128.py $dir/zen.*.${CHUNK}*.${EXT} -C ${CALFILE} -p xx,yy -t aug2014  -d ${TAG1}_245${TAG2}.${CHUNK} -i doc/redundantinfo_PSA64_7ba_7bu_08-15-2014.bin --datapath /data2/home/hz2ug/omnical_old/results  -o /data4/paper/2012EoR/psa_live/PSA64_omnical_results_Aug_2014  --health 3,4 -k
     done;
 done;
 
