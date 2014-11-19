3dc23c48499c569b3f9dafeef1c5b38f7fc40a00
Jeff Zheng <jeff.h.zheng@gmail.com>
2014-11-19 00:24:53 -0800

    classdefinition update in PSA128

--- scripts/omnical_PSA128.py
+++ scripts/omnical_PSA128.py
@@ -14,16 +14,21 @@ class RedundantCalibrator_PAPER(omni.RedundantCalibrator):
         nTotalAnt = len(aa)
         omni.RedundantCalibrator.__init__(self, nTotalAnt)
         self.aa = aa
-
-    def compute_redundantinfo(self, badAntenna = [], badUBL = [], antennaLocationTolerance = 1e-6):
-        self.antennaLocationTolerance = antennaLocationTolerance
-        self.badAntenna = badAntenna
-        self.badUBL = badUBL
         self.antennaLocation = np.zeros((self.nTotalAnt,3))
         for i in range(len(self.aa.ant_layout)):
             for j in range(len(self.aa.ant_layout[0])):
                 self.antennaLocation[self.aa.ant_layout[i][j]] = np.array([i, j, 0])
         self.preciseAntennaLocation = np.array([ant.pos for ant in self.aa])
+        self.badAntenna = []
+        self.badUBLpair = []
+        for i in range(nTotalAnt):
+            if i not in self.aa.ant_layout.flatten():
+                self.badAntenna += [i]
+                
+    def compute_redundantinfo(self, badAntenna = [], badUBLpair = [], antennaLocationTolerance = 1e-6):
+        self.antennaLocationTolerance = antennaLocationTolerance
+        self.badAntenna += badAntenna
+        self.badUBLpair += badUBLpair
         omni.RedundantCalibrator.compute_redundantinfo(self)
 
 
