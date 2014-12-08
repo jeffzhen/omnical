c4be1fdcae6b331cbcfd9b4c649d202050de75a5
Jeff Zheng <jeff.h.zheng@gmail.com>
2014-12-08 17:26:18 -0500

    IO improvement for omnical2npz

--- scripts/omnical2npz.py
+++ scripts/omnical2npz.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 import numpy as n
-import sys, optparse
+import sys, optparse, os
 import omnical.calibration_omni as omni
 
 
@@ -11,9 +11,10 @@ if __name__ == '__main__':
 
     o = optparse.OptionParser()
 
-    o.add_option('-i', '--infopath', action = 'store', default = '/data2/home/hz2ug/omnical/doc/redundantinfo_PSA128_17ba.bin', help = 'redundantinfo file to read')
+    o.add_option('-i', '--infopath', action = 'store', default = '/data2/home/cacheng/calibrated128/psa6620/psa6620config3.bin', help = 'redundantinfo file to read')
     o.add_option('-a', '--nAntenna', action = 'store', type = 'int', default = 128, help = 'Number of antennas.')
     o.add_option('-f', '--nFrequency', action = 'store', type = 'int', default = 203, help = 'Number of frequency bins.')
+    o.add_option('-o', '--outputpath', action = 'store', default = None, help = 'output folder. Same folder by default.')
     o.add_option('--overwrite', action = 'store_true', help = 'overwrite if file exists')
     opts,args = o.parse_args(sys.argv[1:])
 
@@ -35,6 +36,13 @@ if __name__ == '__main__':
     for omnical_file in sys.argv[1:]:
         print "Processing", omnical_file, "..."
         sys.stdout.flush()
+
+        if opts.outputpath is None:
+            oppath = omnical_file + '.npz'
+        elif not (os.path.isdir(opts.outputpath) and os.access(opts.outputpath, os.W_OK | os.X_OK)):
+            raise IOError("Cannot write to %s."%opts.outputpath)
+        else:
+            oppath = opts.outputpath + '/' + omnical_file.split('/')[-1] + '.npz'
         pol = omnical_file.split('.')[-2].split('_')[-1][0]
         d = n.fromfile(omnical_file, dtype=n.float32)
         ntime = d.size / NCHAN / NPRM
@@ -46,4 +54,4 @@ if __name__ == '__main__':
         g = 10**d[:,:,3:3+NANT] * n.exp(- 1.j * d[:,:,3+NANT:3+2*NANT]) # minus to change conjugation convention between omnical and aipy
         for i in xrange(NANT):
             d_npz['%d,%s' % (calibrator.Info.subsetant[i],pol)] = g[...,i]
-        n.savez(omnical_file+'.npz', **d_npz)
+        n.savez(oppath, **d_npz)
