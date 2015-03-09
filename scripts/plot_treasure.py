13ec4bd2c04e2782224b92d3bb691f0712fa57ff
Jeff Zheng <jeff_z@mit.edu>
2015-03-09 13:58:59 -0400

    minor bug fix

--- scripts/plot_treasure.py
+++ scripts/plot_treasure.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import numpy as np
 import commands, os, time, math, ephem
 import omnical.calibration_omni as omni
@@ -9,11 +11,20 @@ treasure = omni.Treasure(sys.argv[1])
 item = sys.argv[2]
 if item not in ['count', 'variance_re', 'variance_im', 'weighted_mean', 'mean', 'weighted_variance']:
 	print "%s not recognized."%item
+if item in ['mean', 'weighted_mean']:
+	if len(sys.argv) < 4 or sys.argv[3] not in ['amp', 'phs']:
+		print "Please specify amp or phs"
+
 for p, pol in enumerate(treasure.ubls.keys()):
 	c = treasure.get_coin_now((pol,treasure.ubls[pol][np.argsort(np.linalg.norm(treasure.ubls[pol], axis=1))[0]]))
 
 	plt.subplot('1%i%i'%(len(treasure.ubls.keys()), p+1))
 	data = c.__getattr__(item)
+	if item in ['mean', 'weighted_mean']:
+		if sys.argv[3] == 'amp':
+			data = np.abs(data)
+		else:
+			data = np.angle(data)
 	flag = np.isnan(data)|np.isinf(data)
 	plt.imshow(data, aspect = 1/5., interpolation='none', vmin = np.percentile(data[~flag], 5), vmax = np.percentile(data[~flag], 95));plt.colorbar();plt.title(pol)
 
