93f6967bb2937c5577aee9c88486341efa001298
Jeff Zheng <jeff_z@mit.edu>
2015-03-24 14:48:14 -0400

    bug fix

--- scripts/first_cal.py
+++ scripts/first_cal.py
@@ -163,10 +163,10 @@ else:
 
 	for p, pol in enumerate(wantpols.keys()):
 		if p == 0:
-			rawdata, t, timing, lst, rawflag = omni.importuvs(uvfiles_dic[pol], wantpols, totalVisibilityId = np.concatenate([[[i,j] for i in range(j + 1)] for j in range(len(aa))]), timingTolerance=100)
+			rawdata, t, timing, lst, rawflag = omni.importuvs([uvfiles_dic[pol]], wantpols, totalVisibilityId = np.concatenate([[[i,j] for i in range(j + 1)] for j in range(len(aa))]), timingTolerance=100)
 
 		else:
-			tmpdata, t, timing, lst, tmpflag = omni.importuvs(uvfiles_dic[pol], {pol:wantpols[pol]}, totalVisibilityId = np.concatenate([[[i,j] for i in range(j + 1)] for j in range(len(aa))]), timingTolerance=100)
+			tmpdata, t, timing, lst, tmpflag = omni.importuvs([uvfiles_dic[pol]], {pol:wantpols[pol]}, totalVisibilityId = np.concatenate([[[i,j] for i in range(j + 1)] for j in range(len(aa))]), timingTolerance=100)
 			rawdata = np.concatenate((rawdata, tmpdata))
 print FILENAME + " MSG:",  len(t), "slices read."
 sys.stdout.flush()
